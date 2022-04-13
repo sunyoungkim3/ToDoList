7b25a76e9ee77d139bb7aa0cf6d0deba5a7d3a16
sunyoungkim3 <sykim3@seegene.com>
2022-04-13 17:15:29 +0900

    array를 가져와서 li로 보여준다

--- src/App.js
+++ src/App.js
@@ -11,8 +11,12 @@ function App() {
     if(toDo === ""){
       return;
     }
-    setToDo("")
-    setToDos((currentArray) =>[toDo, ...currentArray]);
+//수정하는 함수를 사용 할 때 두가지 옵션
+    setToDo("") //직접 값을 입력
+    setToDos((currentArray) =>[toDo, ...currentArray]); 
+    //함수를 보내는 방법 첫번째 인자: 현재 State. 이를 이용하여 새로운 State를 만드는데 사용
+    // 현재 toDos를 불러와서 새로운 toDo의 array로 return
+
   }; 
 
   console.log(toDos);
@@ -29,6 +33,14 @@ function App() {
         />
         <button>ADD TO DO</button>
       </form>
+      <hr />
+      {toDos.map((item, index)=> (
+      <li key={index}>{item}</li>
+      ))}  
+
+      {/* map: array에 있는 item을 내가 원하는것으로 바꿔주는 역할. 새로운 array로 return 해줌 */}
+      {/* map((item)=>" "), 첫번째 인자에 item을 받아올 수 있음*/}
+
     </div>
   );
 }
